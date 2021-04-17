75b79dd0a89724ca5a631972bfa8de9e9564dee4
MMonteiro <a21801093@alunos.ulht.pt>
2021-04-17 23:42:37 +0100

    ficha 08.

--- app/src/main/java/cm/mmonteiro/acalculator/views/calculator/CalculatorFragment.kt
+++ app/src/main/java/cm/mmonteiro/acalculator/views/calculator/CalculatorFragment.kt
@@ -89,7 +89,7 @@ class CalculatorFragment : Fragment(), OnDisplayChanged {
 
     @Optional
     @OnClick(
-       // R.id.button_00,
+        R.id.button_00,
         R.id.button_1, R.id.button_2, R.id.button_3, R.id.button_4,
         R.id.button_5, R.id.button_6, R.id.button_7, R.id.button_8, R.id.button_9,
         R.id.button_ce, R.id.button_backspace, R.id.button_multi, R.id.button_adition,
@@ -115,9 +115,6 @@ class CalculatorFragment : Fragment(), OnDisplayChanged {
             ).show()
         } else {
 
-            //val result = ExpressionBuilder(text_visor.text.toString()).build()
-
-         //   text_visor.text = result.evaluate().toString()
 
            viewModel.onClickEquals()
 
